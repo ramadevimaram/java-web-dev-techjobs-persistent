3138a386489cc607aab139a9308a734cf7b9e43f
ramadevimaram <ramamaram.java@gmail.com>
2020-09-01 19:49:28 -0400

    Final project techjobs-persistent

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,38 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+//will be mapped to one of our tables. Make sure the class has the @Entity annotation
+@Entity
 public class Employer extends AbstractEntity {
+    //should have a string field for location.Add the field for location with validation.
+    @NotNull(message = "Location is required")
+    @Size(min=2, max=100, message = "Location must be between 2 and 100 characters")
+    private String location;
 
+    @OneToMany//(mappedBy = "employer")
+    @JoinColumn
+    private final List<Job> jobs = new ArrayList<>();
 
+    //Hibernate to create an object.
+    public Employer(){}
+
+    // In addition, add getters and setters to Employer.
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,38 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
+    //A field for a longer description of the skill
+    @NotNull(message = "Description is Required")
+    @NotBlank(message = "Skill cannot be blank!")
+    @Size(min=3,max=300, message = "Description must be between 2 and 100 characters")
+    private String description;
+
+    @ManyToMany(mappedBy="skills")
+    private final List<Job> jobs = new ArrayList<>();
+
+    public Skill(){}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
 }
